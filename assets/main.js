8646a15b134ee6ec51afd99d1b13f10d6bd3d161
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-03-29 13:46:26 +0100

    Add function that checks for country code

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,25 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
+const denyList = []
+
+const checkCountryCode = value => {
+  value = value.replace(/[^0-9]/g, "");
+  if (value.length !== 11 || value[0] != 1) {
+    return false
+  } else {
+    return true
+  }
+}
 const validatePhoneNumber = () => {
-  console.log(userInput.value);
+  const value = userInput.value
+  if (!checkCountryCode(value)) {
+    console.log(`invalid Us number: ${value}`)
+    return;
+  }
+
+  console.log(value);
+  userInput.value = ''
 }
 
 checkBtn.addEventListener('click', ()=>{
