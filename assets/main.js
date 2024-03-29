533dbbe3e57757214db2c7bd79287ae7320cdee3
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-03-29 16:57:27 +0100

    Show the validity of a number on the page

--- assets/main.js
+++ assets/main.js
@@ -3,26 +3,35 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
-const matchWithBracket = /^(?:1\s?)?(?:\((\d{3})\)\s?)(\d{3})-(\d{4})$/
-const matchWithHyphen = /^(?:1\s)?(?:(\d{3})-)(\d{3})-(\d{4})$/
-const matchOnlyNumber = /^(?:1\s)?(?:(\d{3})\s?)(\d{3})\s?(\d{4})$/
-const validList = [matchWithBracket, matchWithHyphen, matchOnlyNumber]
+const matchWithBracket = /^(?:1\s?)?(?:\((\d{3})\)\s?)(\d{3})-(\d{4})$/;
+const matchWithHyphen = /^(?:1\s)?(?:(\d{3})-)(\d{3})-(\d{4})$/;
+const matchOnlyNumber = /^(?:1\s)?(?:(\d{3})\s?)(\d{3})\s?(\d{4})$/;
+const validList = [matchWithBracket, matchWithHyphen, matchOnlyNumber];
 
-const isValid = num => validList.some(regex => regex.test(num))
+const isValid = (num) => validList.some((regex) => regex.test(num));
 const validatePhoneNumber = () => {
-  const value = userInput.value
-  let result = isValid(value)
-  console.log(`${result} Us number: value`);
-  userInput.value = ''
-}
+  const value = userInput.value;
+  let validNumber = isValid(value);
 
-checkBtn.addEventListener('click', ()=>{
+  const para = document.createElement("p");
+  if (validNumber) {
+    para.className = "valid";
+    para.innerHTML = `Valid US number: <span>${value}</span>`;
+    result.appendChild(para);
+  } else {
+    para.className = "invalid";
+    para.innerHTML = `Invalid US number: <span>${value}</span>`;
+    result.appendChild(para);
+  }
+  userInput.value = "";
+};
+
+checkBtn.addEventListener("click", () => {
   if (!userInput.value) {
-    alert("Please provide a phone number")
+    alert("Please provide a phone number");
+  } else {
+    validatePhoneNumber();
   }
-  else {
-    validatePhoneNumber()
-  }
-})
+});
 
-  // const Pattern = /^(?:1\s?)?(?:\((\d{3})\)[\s-]?|(\d{3})[\s-]?)(\d{3})[\s-]?(\d{4})$/
+// const Pattern = /^(?:1\s?)?(?:\((\d{3})\)[\s-]?|(\d{3})[\s-]?)(\d{3})[\s-]?(\d{4})$/
